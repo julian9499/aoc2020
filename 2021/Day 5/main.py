1c55581122c482a5c419344635731aab55819a1d
julian <j.m.c.vandijk@student.tudelft.nl>
2022-12-02 10:35:10 +0100

    use dynamic programming for day 6 part 2 WIP

--- 2021/Day 5/main.py
+++ 2021/Day 5/main.py
@@ -1,6 +1,11 @@
 from pull2021 import AocInteraction
-from scipy.sparse import coo_array, csr_matrix
+import matplotlib.pyplot as plt
+import csv
 import numpy as np
+import pandas as pd
+import seaborn as sns
+from scipy.sparse import csr_matrix, coo_array, coo_matrix
+
 
 def part_1(advent_of_code):
     row = []
@@ -23,19 +28,19 @@ def part_1(advent_of_code):
                     row.append(x)
                     col.append(y)
                     data.append(1)
-            else:
-                xstep = 1
-                ystep = 1
-                if coords[0] > coords[2]:
-                    xstep = -1
-                if coords[1] > coords[3]:
-                    ystep = -1
-                x_range = iter(range(coords[0], coords[2]+1, xstep))
-                y_range = iter(range(coords[1], coords[3]+1, ystep))
-                for x in range(0, abs(coords[0]-coords[2])):
-                        row.append(next(x_range))
-                        col.append(next(y_range))
-                        data.append(1)
+            # else:
+            #     xstep = 1
+            #     ystep = 1
+            #     if coords[0] > coords[2]:
+            #         xstep = -1
+            #     if coords[1] > coords[3]:
+            #         ystep = -1
+            #     x_range = iter(range(coords[0], coords[2]+1, xstep))
+            #     y_range = iter(range(coords[1], coords[3]+1, ystep))
+            #     for x in range(0, abs(coords[0]-coords[2])):
+            #             row.append(next(x_range))
+            #             col.append(next(y_range))
+            #             data.append(1)
 
         csr = coo_array((np.array(data), (np.array(row), np.array(col))), shape=(max(row)+1, max(col)+1)).tocsr()
         nnz_inds = csr.nonzero()
@@ -50,8 +55,66 @@ def part_1(advent_of_code):
 
 
 def part_2(advent_of_code):
+    row = []
+    col = []
+    data = []
     with open('input.txt', 'r') as input_file:
-        advent_of_code.answer(2, None)
+        entries = []
+        for line in input_file:
+            s = line.replace(" -> ", ",")
+            coords = list(map(int, s.strip().split(",")))
+            if coords[0] == coords[2]:
+                x = coords[0]
+                for y in range(min(coords[1], coords[3]), max(coords[1], coords[3]) + 1):
+                    row.append(x)
+                    col.append(y)
+                    data.append(1)
+            elif coords[1] == coords[3]:
+                y = coords[1]
+                for x in range(min(coords[0], coords[2]), max(coords[0], coords[2]) + 1):
+                    row.append(x)
+                    col.append(y)
+                    data.append(1)
+            else:
+                xstep = 1
+                ystep = 1
+                if coords[0] > coords[2]:
+                    xstep = -1
+                if coords[1] > coords[3]:
+                    ystep = -1
+                x_range = range(coords[0], coords[2] + xstep, xstep)
+                y_range = range(coords[1], coords[3] + ystep, ystep)
+                for x in range(0, len(x_range)):
+                    row.append(x_range[x])
+                    col.append(y_range[x])
+                    data.append(1)
+
+        coo = coo_matrix((np.array(data), (np.array(row), np.array(col))), shape=(max(row) + 1, max(col) + 1))
+        csr = coo.tocsr()
+        nnz_inds = csr.nonzero()
+        keep = np.where(csr.data != 1)[0]
+        n_keep = len(keep)
+        b = csr_matrix((np.ones(n_keep), (nnz_inds[0][keep], nnz_inds[1][keep])), shape=csr.shape).tocoo()
+
+        df = pd.DataFrame({'x': coo.row,
+                           'y': coo.col,
+                           'value': coo.data})
+        dfOverlap = pd.DataFrame({'x': b.row,
+                           'y': b.col,
+                           'value': b.data})
+
+        # plot
+        pl = sns.scatterplot(data=df, x="x", y="y", s=0.5, alpha=0.5)
+        pl.scatter(data=dfOverlap, x="x", y="y", label="overlap", s=0.25, alpha=0.75)
+        plt.gcf().set_dpi(300)
+        pl.invert_yaxis()
+        pl.xaxis.tick_top()
+        pl.xaxis.set_label_position("top")
+        plt.show()
+
+
+        print(b.todense())
+        advent_of_code.answer(2, b.count_nonzero())
 
 
 if __name__ == "__main__":
--- 2021/Day 6/main.py
+++ 2021/Day 6/main.py
@@ -1,18 +1,71 @@
-from ..pull2021 import AocInteraction
+from pull2021 import AocInteraction
+
 
 
 #  https://adventofcode.com/2021/day/6
 
 def part_1(advent_of_code):
     with open('input.txt', 'r') as input_file:
+        state = map(int, input_file.readline().strip().split(","))
+        for x in range(0, 80):
+            newState = []
+            for fish in state:
+                if fish == 0:
+                    newState.append(8)
+                    newState.append(6)
+                else:
+                    newState.append(fish-1)
+            state = newState
+        # f = open("input_80days.txt", "w")
+        # f.write(str(state))
 
-        advent_of_code.answer(1, None)
+        advent_of_code.answer(1, len(state))
 
 
 def part_2(advent_of_code):
     with open('input.txt', 'r') as input_file:
+        state = list(map(int, input_file.readline().strip().split(",")))
+        newBigState = [] # USE DYNAMIC PROGRAMMING
+        count = 0
+        for i in state:
+            # localstate = [i]
+            count += fish(256, 0, i)
+            print(count)
+            # print(count)
+            # for x in range(0, 43):
+            #     newState = []
+            #     for fish in localstate:
+            #         if fish == 0:
+            #             newState.append(8)
+            #             newState.append(6)
+            #         else:
+            #             newState.append(fish - 1)
+            #     localstate = newState
+            # for j in localstate:
+            #     newBigState.append(j)
 
-        advent_of_code.answer(2, None)
+        # total = 0
+        # print(len(newBigState))
+        # for i in newBigState:
+        #     localstate = [i]
+        #     for x in range(0, 256-123):
+        #         newState = []
+        #         for fish in localstate:
+        #             if fish == 0:
+        #                 newState.append(8)
+        #                 newState.append(6)
+        #             else:
+        #                 newState.append(fish - 1)
+        #         localstate = newState
+        #     # print(localstate)
+        #     total += len(localstate)
+
+        advent_of_code.answer(2, count)
+
+def fish(days, init_time, initial_state):
+    if init_time + initial_state >= days:
+        return 1
+    return fish(days, init_time+initial_state+1, 6) + fish(days, init_time+initial_state+1, 8)
 
 
 if __name__ == "__main__":
